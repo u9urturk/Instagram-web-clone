45d67f7c9a595868434c39766a190ab544d46157
ugur <u9urturk@gmail.com>
2023-02-12 13:33:43 +0300

    refactoring

--- src/pages/message/components/sender.js
+++ src/pages/message/components/sender.js
@@ -22,14 +22,17 @@ function Sender(props) {
   const submitHandle = e => {
     e.preventDefault()
     //console.log(messages)
-    sendMessagesByMessageboxid(messages,props.owner, props.messageBoxId.messageboxid  )
+    sendMessagesByMessageboxid(messages, props.owner, props.messageBoxId.messageboxid)
+    setMessages("")
   }
 
 
   return (
     <form onSubmit={submitHandle} className='h-[42px] border border-gray-300 rounded-l-full   rounded-r-full flex items-center  px-4 w-full max-w-[90%]'>
       <div className='w-[40px] cursor-pointer flex items-center justify-center'><Icon name={"ifade"} size={22}></Icon></div>
-      <div className='w-[420px] '><input onChange={e => setMessages(e.target.value)} className='outline-none w-full px-2' placeholder='Message...'></input></div>
+      <div className='w-[420px] '>
+        <input onChange={e => setMessages(e.target.value)} value={messages} className='outline-none w-full px-2' placeholder='Message...'></input>
+      </div>
       <div className='flex items-center justify-center' >
         <div className={classNames({
           'w-[40px] cursor-pointer flex items-center justify-center ': true,
@@ -39,7 +42,7 @@ function Sender(props) {
           'w-[40px] cursor-pointer flex items-center  justify-center ': true,
           'hidden': !messagesIsEmpty()
         })}><Icon name={"heart"} size={22}></Icon></div>
-        <button type='submit'   className={classNames({
+        <button type='submit' className={classNames({
           'text-facebook ': true,
           'hidden': messagesIsEmpty()
         })}><strong >Send</strong></button>
--- src/pages/message/messagebox.js
+++ src/pages/message/messagebox.js
@@ -1,4 +1,4 @@
-import { onChildAdded, onChildChanged, ref } from 'firebase/database'
+import { child, get, onChildAdded, onChildChanged, ref } from 'firebase/database'
 import React, { useCallback, useEffect, useMemo, useReducer, useState } from 'react'
 import { act } from 'react-dom/test-utils'
 import { useSelector } from 'react-redux'
@@ -11,20 +11,28 @@ import Messages from './components/messages'
 import Sender from './components/sender'
 
 export default function MessageBox() {
- // console.count("app rendered")
+  // console.count("app rendered")
 
   const messageboxid = useParams();
   const user = useSelector(state => state.auth.user)
   const msRef = ref(returntools(), `/messages/${messageboxid.messageboxid}`)
+  const mbRef = ref((returntools()), `/messageboxes/${messageboxid.messageboxid}`)
 
   function reducer(rstate, action) {
     switch (action.type) {
-  
+
       case 'SET_MESSAGES':
 
         return {
           ...rstate,
           messages: action.value
+        }
+
+      case 'SET_MEMBERS':
+
+        return {
+          ...rstate,
+          members: action.value
         }
 
 
@@ -39,87 +47,101 @@ export default function MessageBox() {
 
 
   const [rstate, dispatch] = useReducer(reducer, {
-    messages: []
-   })
+    messages: [],
+    members: []
+  })
+
+
+ 
 
   const getMessages = useCallback(async (messages) => {
     const dynMessages = [];
-      messages.forEach(message => {
+    messages.forEach(message => {
 
-        if (message.owner == user.uid) {
-          let owner = {
-            owner: true,
-            message: message.message,
-            time: message.time
-          }
-
-          dynMessages.push(owner);
-        } else {
-
-          let xmember = {
-            owner: false,
-            message: message.message,
-            time: message.time
-          }
-
-          dynMessages.push(xmember);
-
+      if (message.owner == user.uid) {
+        let owner = {
+          owner: true,
+          message: message.message,
+          time: message.time
         }
-      })
-    
-      //console.log(dynMessages)
+
+        dynMessages.push(owner);
+      } else {
+
+        let xmember = {
+          owner: false,
+          message: message.message,
+          time: message.time
+        }
+
+        dynMessages.push(xmember);
+
+      }
+    })
+
+    //console.log(dynMessages)
     return dynMessages;
-    
+
 
   })
 
 
   useEffect(() => {
-   
-    
-    const run = ()=>{
+
+
+    const run = () => {
       const messages = []
-      const unsub1= onChildAdded(msRef, async (snapshot) => {
+      const unsub3 =  get(child(ref(returntools()), `messageboxes/${messageboxid.messageboxid}`)).then((snapshot)  => {
+
+        // dispatch({
+        //   type:'SET_MEMBERS',
+        //   value:res
+        // })
+        console.log(snapshot.val());
+  
+      });
+      const unsub1 = onChildAdded(msRef, async (snapshot) => {
 
         messages.push(snapshot.val())
-        await getMessages(messages).then(res=>{
+        await getMessages(messages).then(res => {
           dispatch({
-            type:'SET_MESSAGES',
-            value:res
+            type: 'SET_MESSAGES',
+            value: res
           })
         })
 
-       
+
       });
-      const unsub2= onChildChanged(msRef, async (snapshot) => {
+      const unsub2 = onChildChanged(msRef, async (snapshot) => {
         messages.push(snapshot.val());
-        await getMessages(messages).then(res=>{
+        await getMessages(messages).then(res => {
           dispatch({
-            type:'SET_MESSAGES',
-            value:res
+            type: 'SET_MESSAGES',
+            value: res
           })
         })
         //console.log(snapshot.val());
 
       });
 
-      
 
-      return()=>{
+
+      return () => {
         unsub1();
         unsub2();
+        unsub3();
       }
 
-    
+
 
     }
 
     messageboxid.messageboxid && run();
-    
+
 
   }, [messageboxid])
 
-console.log(rstate.messages)
+  //console.log(rstate.messages)
 
   //console.count(member)
   //console.log(rstate.newMember)
@@ -136,7 +158,7 @@ console.log(rstate.messages)
       </div>
       {/* Input */}
       <div className='w-full flex items-center justify-center h-[84px] '>
-        <Sender messageBoxId={messageboxid} owner={user.uid}/>
+        <Sender messageBoxId={messageboxid} owner={user.uid} />
       </div>
     </div>
   )
